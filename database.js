26e58b442e4d5d041ed92a4d80b57d818558cad2
= <vinhvipvl@gmail.com>
2021-08-21 17:04:55 +0700

    config heroku

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "123456",
-    database: "book_db",
-    host: "localhost",
+    host: 'ec2-35-153-114-74.compute-1.amazonaws.com',
+    database: "dt434phd71r4",
+    user: "pidtkoivbjludp",
+    password: "e0bce354b6922538b3e37c7d351d41f150b9a39d34a70b47e26bcdaeea4a1abf",
     port: 5432
 });
 
