0f9a1ffbdf410835782a16def512726054544b30
= <vinhvipvl@gmail.com>
2021-08-21 21:47:30 +0700

    fix

--- app.js
+++ app.js
@@ -21,14 +21,6 @@ app.use((req, res, next) => {
     next();
 })
 
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 app.use('/book', bookRouter);
 
 app.use('/test', (req, res) => {
