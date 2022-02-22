0037cf083f7b3e65fc75c6accd1e6b8dbe922675
Vinh <vinhvipvl@gmail.com>
2022-02-22 10:18:47 +0700

    node run server

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 const db = require('./database');
 
 // Swagger UI
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 dotenv.config();
 
